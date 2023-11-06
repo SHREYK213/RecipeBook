6a8175207a187940c1db0b896aadbb5c0ab14340
SHREYK213 <shreyaspoojary240@gmail.com>
2023-11-06 13:14:19 +0530

    Services added and shopping services added to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({
   providedIn: 'root',
@@ -7,14 +9,24 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   private recipe: Recipe[] = [
     new Recipe(
-      'A test Recipe',
-      'This is a desc',
-      'https://www.vegrecipesofindia.com/wp-content/uploads/2021/09/malpua-1-1024x1536.jpg'
+      'Spaghetti',
+      'Its just spaghetti',
+      'https://urbanblisslife.com/wp-content/uploads/2023/01/Jolibee-Spaghetti-FEATURE.jpg',
+      [
+        new Ingredient('Spaghetti', 1),
+        new Ingredient('Sauce', 1),
+        new Ingredient('Parsle', 1),
+      ]
     ),
     new Recipe(
-      'A test Another Recipe',
-      'This is another desc',
-      'https://www.vegrecipesofindia.com/wp-content/uploads/2021/09/malpua-1-1024x1536.jpg'
+      'Extra Large Burger ',
+      'A large Burger',
+      'https://www.eatthis.com/wp-content/uploads/sites/4/2023/02/Wendys-Big-Bacon-Classic-Triple.jpg?quality=82&strip=all',
+      [
+        new Ingredient('Buns', 1),
+        new Ingredient('Meat', 1),
+        new Ingredient('Mayo', 1),
+      ]
     ),
   ];
 
@@ -23,5 +35,9 @@ export class RecipeService {
   getRecipes() {
     return this.recipe.slice();
   }
-  constructor() {}
+  constructor(private slService: ShoppingListService) {}
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,11 +8,14 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomato', 10),
-  ];
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+  ingredients: Ingredient[];
+
+  constructor(private slService: ShoppingListService) {}
+
+  ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => (this.ingredients = ingredients)
+    );
   }
 }
